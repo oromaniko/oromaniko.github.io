9d6a24523efeb7ee0fd29178e5627566d08b65d8
Olga Romaniko <oromaniko@gmail.com>
2022-10-26 13:51:14 +0300

    add store

---
+++ src/store/index.ts
@@ -0,0 +1,20 @@
+import {
+    applyMiddleware,
+    combineReducers,
+    legacy_createStore as createStore,
+} from 'redux'
+import thunk from 'redux-thunk'
+import reducers from './reducers'
+import { useDispatch } from 'react-redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+
+const rootReducer = combineReducers(reducers)
+
+export const store = createStore(
+    rootReducer,
+    composeWithDevTools(applyMiddleware(thunk))
+)
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
+export const useAppDispatch = () => useDispatch<AppDispatch>()
