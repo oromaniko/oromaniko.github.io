7dcf4f1035ddc4c130754358ec3c088e7e90e04d
Olga Romaniko <oromaniko@gmail.com>
2022-10-29 00:39:14 +0300

    add footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,32 @@
 import React from 'react'
+import styled from 'styled-components'
 
 const Footer = () => {
-    return <div>FOOTER</div>
+    return (
+        <footer>
+            <Link href='https://github.com/gothinkster/angularjs-realworld-example-app'>
+                Fork on GitHub
+            </Link>
+        </footer>
+    )
 }
 
 export default Footer
+
+const Link = styled.a`
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+    background: linear-gradient(#485563, #29323c);
+    text-align: center;
+    padding: 15px;
+    box-shadow: 0 5px 5px 5px rgb(0 0 0 / 40%);
+    z-index: 999;
+    color: #fff;
+    font-size: 1.5rem;
+    display: block;
+
+    :hover {
+        text-decoration: underline;
+    }
+`
