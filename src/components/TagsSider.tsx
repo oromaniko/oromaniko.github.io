d3ad9fb047052d00e0da1456c7e885bc49988549
Olga Romaniko <oromaniko@gmail.com>
2022-10-31 11:48:43 +0300

    add tags sider

---
+++ src/components/TagsSider.tsx
@@ -0,0 +1,68 @@
+import React from 'react'
+import { Tag } from '../models/tags'
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+import { centered } from '../mixins'
+
+type props = {
+    tags: Tag[]
+    selected: Tag | ''
+    isLoading: boolean
+    handleSearchTag: (tag: Tag) => void
+}
+
+const TagsSider = ({ tags, isLoading, handleSearchTag, selected }: props) => {
+    return (
+        <Sidebar>
+            <p>Popular Tags</p>
+            {isLoading ? (
+                'Loading tags...'
+            ) : (
+                <TagList>
+                    {tags.map((tag) => (
+                        <TagItem
+                            isSelected={tag === selected}
+                            key={tag}
+                            to={'#'}
+                            onClick={() => handleSearchTag(tag)}
+                        >
+                            {tag}
+                        </TagItem>
+                    ))}
+                </TagList>
+            )}
+        </Sidebar>
+    )
+}
+
+export default TagsSider
+
+const Sidebar = styled.div`
+    padding: 5px 10px 10px 10px;
+    background: #f3f3f3;
+    border-radius: 4px;
+
+    p {
+        margin-bottom: 0.2rem;
+    }
+`
+
+const TagList = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    gap: 0.2rem;
+`
+
+const TagItem = styled(Link)<{ isSelected: boolean }>`
+    ${centered};
+    color: #fff;
+    font-size: 0.8rem;
+    padding: 0.1rem 0.6em;
+    background-color: ${(props) => (props.isSelected ? '#687077' : '#818a91')};
+    text-decoration: ${(props) => (props.isSelected ? 'underline' : 'none')};
+    border-radius: 10rem;
+
+    :hover {
+        background-color: #687077;
+    }
+`
