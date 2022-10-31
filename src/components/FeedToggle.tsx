9a8cba6243befca8354aaad3f35e01969f2cb992
Olga Romaniko <oromaniko@gmail.com>
2022-10-31 11:52:11 +0300

    add FeedToggle

---
+++ src/components/FeedToggle.tsx
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from 'react'
+import { Tag } from '../models/tags'
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+import { useActions } from '../hooks/useActions'
+
+type props = {
+    tag: Tag | ''
+    setTag: (tag: Tag | '') => void
+}
+const FeedToggle = ({ tag, setTag }: props) => {
+    const { fetchArticles } = useActions()
+    const [activeItem, setActiveItem] = useState('Global')
+
+    useEffect(() => {
+        if (tag) {
+            setActiveItem('tag')
+        }
+    }, [tag])
+
+    const handleClickGlobalFeed = () => {
+        fetchArticles(10)
+        setActiveItem('Global')
+        setTag('')
+    }
+
+    return (
+        <Container>
+            <ul>
+                <li>
+                    <FeedLink
+                        to={'#'}
+                        isActive={activeItem === 'Global'}
+                        onClick={handleClickGlobalFeed}
+                    >
+                        Global Feed
+                    </FeedLink>
+                </li>
+                {tag && (
+                    <li>
+                        <FeedLink
+                            to={'#'}
+                            isActive={true}
+                        >{`# ${tag}`}</FeedLink>
+                    </li>
+                )}
+            </ul>
+        </Container>
+    )
+}
+
+export default FeedToggle
+
+const Container = styled.div`
+    margin-bottom: -1px;
+
+    li {
+        margin-left: 0.2rem;
+        display: inline-flex;
+    }
+`
+
+const FeedLink = styled(Link)<{ isActive: boolean }>`
+    padding: 0.5em 1em;
+    border-bottom: ${(props) =>
+        props.isActive ? '2px solid #5cb85c' : 'none'};
+    color: ${(props) => (props.isActive ? '#5CB85C' : '#aaa')};
+`
